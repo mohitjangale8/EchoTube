00e6d0a246bcae7e1938e2b429e7666f5f3e4893
Mohit <mohitjangale89@gmail.com>
2025-06-26 23:37:26 +0530

    Utils Error , Response, Handler

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is Running at Port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB Connection Failed !!" , error)
+})
